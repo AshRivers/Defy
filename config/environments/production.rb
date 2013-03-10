a96d2d0d6300586855ec72811a817dd56efee85d
Ash <maildoesnotexist@gmail.com>
2013-03-10 21:09:48 +0100

    precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,8 @@ config.assets.precompile += %w( .svg .eot .woff .ttf .js .css )
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
