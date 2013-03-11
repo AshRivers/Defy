ee1dd9aba4ff3c101603a826b9b86076535041b9
Ash <maildoesnotexist@gmail.com>
2013-03-11 02:59:33 +0100

    change of limit

--- db/migrate/20130311005344_change_column_type_to_text.rb
+++ db/migrate/20130311005344_change_column_type_to_text.rb
@@ -1,9 +1,9 @@
 class ChangeColumnTypeToText < ActiveRecord::Migration
   def up
-  	change_column :storypoints, :story, :text
+  	change_column :storypoints, :story, :text, :limit => nil
   end
 
   def down
-  	change_column :storypoints, :story, :string
+  	change_column :storypoints, :story, :string, :limit => 255
   end
 end
