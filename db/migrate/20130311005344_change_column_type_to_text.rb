c9c0bd428562b52eb04fa21868940153f264f3e8
Ash <maildoesnotexist@gmail.com>
2013-03-13 00:41:25 +0100

    without cropping

--- db/migrate/20130311005344_change_column_type_to_text.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnTypeToText < ActiveRecord::Migration
-  def up
-  	change_column :storypoints, :story, :text, :limit => nil
-  end
-
-  def down
-  	change_column :storypoints, :story, :string, :limit => 255
-  end
-end
